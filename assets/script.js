8eee87474702181e32cf7e23eea6d49fd0f51807
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-05-27 20:41:51 -0300

    Finalizando projeto

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
+function insert(num) {
+  let number = document.getElementById('result').innerHTML
 
+  document.getElementById('result').innerHTML = number + num
+}
+
+function clean() {
+  document.getElementById('result').innerHTML = ''
+}
+
+function delet() {
+  let result = document.getElementById('result').innerHTML
+
+  document.getElementById('result').innerHTML = result.substring(
+    0,
+    result.length - 1
+  )
+}
+
+function calculate() {
+  let result = document.getElementById('result').innerHTML
+  if (result) {
+    document.getElementById('result').innerHTML = eval(result)
+  }
+}
